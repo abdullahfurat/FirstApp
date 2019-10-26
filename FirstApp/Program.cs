264d9b79d8b496480b56aedd5121657e2a2a2300
abdullahfurat <a_furat@hotmail.com>
2019-10-26 12:14:25 +0300

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            //Console.ReadKey();
+            Console.WriteLine("Lütfen Adınızı Giriniz : ");
+            Console.WriteLine("Giriş Yapan kullanıcı : " + Console.ReadLine());
+            Console.ReadKey();
+        }
+    }
+}
